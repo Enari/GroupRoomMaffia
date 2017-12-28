1900b02c42d711d99ee3e920cbd14bc08a7d0511
Anton Roslund <enari@minit.nu>
2017-12-28 15:19:54 +0100

    Added CSRF Token to header

--- resources/views/layouts/subviews/headder.blade.php
+++ resources/views/layouts/subviews/headder.blade.php
@@ -5,3 +5,6 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
