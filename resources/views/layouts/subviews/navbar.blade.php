ef12cc7bde9d6af6059cfa2810037ac3de9fb2ef
Anton Roslund <enari@minit.nu>
2017-12-29 10:14:39 +0100

    Added links and auth check to navbar

--- resources/views/layouts/subviews/navbar.blade.php
+++ resources/views/layouts/subviews/navbar.blade.php
@@ -8,20 +8,39 @@
       <li class="nav-item">
         <a class="nav-link" href="/">Home</a>
       </li>
+@auth
+      <li class="nav-item">
+        <a class="nav-link" href="/bookings">Bookings</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Schedulled bookings</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Users</a>
+      </li>
       <li class="nav-item">
         <a class="nav-link" href="/sessions">Sessions</a>
       </li>
+@endauth
     </ul>
     <ul class="navbar-nav">
+@auth
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Profile
+          {{ \Auth::user()->username }}
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
           <a class="dropdown-item" href="/logout">Logout</a>
         </div>
       </li>
+@endauth
+@guest
+      <li class="nav-item">
+        <a class="nav-link" href="/login">Login</a>
+      </li>
+@endguest
     </ul>
+
   </div>
 </nav>
 <div class="container-fluid" style="padding-bottom: 1rem"></div> {{-- Ugly hack to get the padding to work... --}}
