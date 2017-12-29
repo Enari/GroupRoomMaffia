e0f1a305853c2f131c0b824e8a53035e27b6c2d7
Anton Roslund <enari@minit.nu>
2017-12-29 16:12:47 +0100

    Added Model for Booking

---
+++ app/Models/Booking.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+
+use App\Helpers\KronoxCommunicator;
+
+class Booking
+{
+    public $bookingID;
+    public $date;
+    public $time;
+    public $booker;
+    public $room;
+    public $message;
+}
