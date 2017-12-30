088fb7f6195ad628d00aad0f0fe13ac6db44f116
Anton Roslund <enari@minit.nu>
2017-12-30 13:27:50 +0100

    Added laracasts flash

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //for flashing alearts
+        Laracasts\Flash\FlashServiceProvider::class,
+
     ],
 
     /*
