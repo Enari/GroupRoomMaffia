e2b8894a6ea2e798e61c934c256ba88c0db36c0e
Anton Roslund <enari@minit.nu>
2018-01-07 17:15:24 +0100

    Changed back Bootstrap version

--- resources/views/layouts/subviews/header.blade.php
+++ resources/views/layouts/subviews/header.blade.php
@@ -3,7 +3,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         {{-- Fonts --}}
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
         <!-- CSRF Token -->
