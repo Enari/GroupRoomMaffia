e97372305ffaa1e01c8cca583e837f35057c1ff6
Anton Roslund <enari@minit.nu>
2018-01-15 11:01:35 +0100

    Recurring test

--- app/Models/SchedulledBooking.php
+++ app/Models/SchedulledBooking.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use App\Helpers\KronoxCommunicator;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,15 +10,32 @@ class SchedulledBooking extends Model
 {
     public function book()
     {
-        $url = 'https://webbschema.mdh.se/ajax/ajax_resursbokning.jsp?op=boka';
-        $url = $url.'&datum='.urlencode(substr($this->date, 2, 8));
-        $url = $url.'&id='.urlencode($this->room);
-        $url = $url.'&typ=RESURSER_LOKALER';
-        $url = $url.'&intervall='.urlencode($this->time);
-        $url = $url.'&moment='.(empty($this->message) ? '%20' : urlencode($this->message));
-        $url = $url.'&flik=FLIK_0001';
+        $session = KronoxSession::where(['sessionActive' => 1, 'MdhUsername' => $this->booker])->first();
 
-        $this->result = KronoxCommunicator::httpGet($url, KronoxSession::where(['sessionActive' => 1, 'MdhUsername' => $this->booker])->first()->JSESSIONID);
+        if(empty($session)) {
+            $this->result = 'No active session';
+        }
+        else {
+            $url = 'https://webbschema.mdh.se/ajax/ajax_resursbokning.jsp?op=boka';
+            $url = $url.'&datum='.urlencode(substr($this->date, 2, 8));
+            $url = $url.'&id='.urlencode($this->room);
+            $url = $url.'&typ=RESURSER_LOKALER';
+            $url = $url.'&intervall='.urlencode($this->time);
+            $url = $url.'&moment='.(empty($this->message) ? '%20' : urlencode($this->message));
+            $url = $url.'&flik=FLIK_0001';
+
+            $this->result = KronoxCommunicator::httpGet($url, $session->JSESSIONID);
+        }
+
         $this->save();
+
+        // Create new booking if recurring
+        if($this->recurring == 1)
+        {
+            $new = $this->replicate();
+            $new->date = Carbon::parse($new->date)->addWeek()->toDateString();
+            $new->result = NULL;
+            $new->save();
+        }
     }
 }
